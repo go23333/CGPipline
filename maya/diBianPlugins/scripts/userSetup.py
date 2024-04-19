9d18e432efd1b85eafc4eb6dfd4ebb73c996eb79
unknown <30623050+timesups@users.noreply.github.com>
2024-04-19 16:41:27 +0800

    修正因为path顺序的问题导致插件不可用的问题

--- maya/diBianPlugins/scripts/userSetup.py
+++ maya/diBianPlugins/scripts/userSetup.py
@@ -1,12 +1,17 @@
 #coding=utf-8
 #Init maya menu
+
 from mayaTools import reloadModule
 reloadModule()
 
-
+import sys
+for path in sys.path:
+    if "vendor" in path:
+        break
+sys.path.remove(path)
+sys.path.insert(0,path)
 
 from pymel.core.general import evalDeferred
-
 
 def installMenu():
     import mayaTools.menu
