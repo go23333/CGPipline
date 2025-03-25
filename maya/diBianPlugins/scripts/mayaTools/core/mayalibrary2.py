8195dd10ecff1b8215ad15ead93a19773d1e535d
timesups <30623050+timesups@users.noreply.github.com>
2025-03-25 16:00:32 +0800

    修正导出时会导出当前场景所有模型的bug

--- maya/diBianPlugins/scripts/mayaTools/core/mayalibrary2.py
+++ maya/diBianPlugins/scripts/mayaTools/core/mayalibrary2.py
@@ -8,8 +8,13 @@ pm.loadPlugin( 'fbxmaya.mll' )
 
 
 
-def export_fbx(obj,path,op):
-    pm.mel.FBXExport(obj,f=path,pr=1)
+def export_fbx(obj,path,op=""):
+    current_select = cmds.ls(sl=1)
+    cmds.select(cl=1)
+    cmds.select(obj)
+    pm.mel.FBXExport(f=path,pr=1,op=op,s=1)
+    cmds.select(cl=1)
+    cmds.select(current_select)
 
 def import_fbx(path,rpr):
     pm.mel.FBXImport(f=path,ignoreVersion=1,mergeNamespacesOnClash=0,rpr=rpr, pr=1)
@@ -19,6 +24,7 @@ def import_fbx(path,rpr):
 if __name__ == '__main__':
     from mayaTools import reloadModule
     reloadModule()
+    export_fbx("pCube2","d:/test.fbx")
 
     
 
